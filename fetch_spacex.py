5cfae1fbe74e90441272a9691bfbc79ad59b0484
pn00m <lunueb@gmail.com>
2022-05-09 16:01:21 +0300

    change API link, payload

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,19 +1,19 @@
 import os
 
-
 import requests
+
 from download_pictures import download_pictures
 
 
 def main():
     path = 'images'
-    spacex_api_link = 'https://api.spacexdata.com/v3/rockets'
-    payload = {'rocket_id': 'falcon_heavy', 'limit': 1, 'offset': 2}
+    spacex_api_link = 'https://api.spacexdata.com/v3/launches'
+    payload = {'flight_number': '108'}
     response = requests.get(spacex_api_link, params=payload)
-    spacex_images = response.json()[0]['flickr_images']
+    spacex_images = response.json()[0]['links']['flickr_images']
     os.makedirs(path, exist_ok=True)
     for image_number, spacex_image_url in enumerate(spacex_images):
-        spacex_filename = '{}/spacex{}.jpg'.format(path, image_number + 1)
+        spacex_filename = '{}/spacex{}.jpg'.format(path, image_number+1)
         download_pictures(spacex_image_url, spacex_filename)
 
 
