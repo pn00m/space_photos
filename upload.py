2567a675593117bebdddc018317a1e46dca0c7f8
pn00m <lunueb@gmail.com>
2022-05-09 12:07:15 +0300

    Replace os.environ to os.getenv

--- upload.py
+++ upload.py
@@ -11,10 +11,7 @@ def main():
     bot_token = os.environ['TELEGRAM_BOT_TOKEN']
     channel_id = os.environ['TELEGRAM_CHANNEL_ID']
     bot = telegram.Bot(token=bot_token)
-    try:
-        cycle_delay = os.environ['DELAY']
-    except KeyError:
-        cycle_delay = 86400
+    cycle_delay = os.getenv('DELAY', 86400)
     for filenames in os.walk(path):
         for filename in filenames[2]:
             time.sleep(int(cycle_delay))
