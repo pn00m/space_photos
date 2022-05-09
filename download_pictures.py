c47644e89bdf6e6ce2966c261f64c2ab4c5d451e
pn00m <lunueb@gmail.com>
2022-05-09 16:06:35 +0300

    Remove exception handling

--- download_pictures.py
+++ download_pictures.py
@@ -1,9 +1,7 @@
 import requests
 
 def download_pictures(url, filename):
-    try:
-        response = requests.get(url)
-        with open(filename, 'wb') as file:
-            file.write(response.content)
-    except requests.exceptions.ConnectionError:
-        pass
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filename, 'wb') as file:
+        file.write(response.content)
