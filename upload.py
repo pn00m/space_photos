c98e609e9ba3d2ea0e369a8c60382b16af2d69e2
pn00m <lunueb@gmail.com>
2022-05-12 15:03:11 +0300

    Simplify code (cycles, indexes) in upload.py

--- upload.py
+++ upload.py
@@ -12,9 +12,10 @@ def main():
     channel_id = os.environ['TELEGRAM_CHANNEL_ID']
     bot = telegram.Bot(token=bot_token)
     cycle_delay = os.getenv('DELAY', 86400)
-    for filenames in os.walk(path):
-        for filename in filenames[2]:
+    for root_dir, folders, filenames in os.walk(path):
+        for filename in filenames:
             with open(path+'/'+filename, 'rb') as space_photo_file:
+                print(space_photo_file)
                 bot.send_photo(channel_id, space_photo_file)
                 time.sleep(int(cycle_delay))
             os.remove(path+'/'+filename)
