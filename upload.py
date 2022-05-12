5d35f3247b3aecc4c1d0073234eb804d1a5dbd96
pn00m <lunueb@gmail.com>
2022-05-12 16:36:02 +0300

    Invent os.path.join in upload.py

--- upload.py
+++ upload.py
@@ -14,11 +14,10 @@ def main():
     cycle_delay = os.getenv('DELAY', 86400)
     for root_dir, folders, filenames in os.walk(path):
         for filename in filenames:
-            with open(path+'/'+filename, 'rb') as space_photo_file:
-                print(space_photo_file)
+            with open(os.path.join(path, filename), 'rb') as space_photo_file:
                 bot.send_photo(channel_id, space_photo_file)
                 time.sleep(int(cycle_delay))
-            os.remove(path+'/'+filename)
+            os.remove(os.path.join(path, filename))
 
 
 if __name__ == '__main__':
