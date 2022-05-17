ae3d6e1530fe40a05216f7db5faf4f527016956a
pn00m <lunueb@gmail.com>
2022-05-17 23:15:01 +0300

    make unpacking

--- define_file_extension.py
+++ define_file_extension.py
@@ -5,5 +5,5 @@ from urllib.parse import urlsplit
 def define_file_extension(url):
     url_path = urlsplit(url).path
     path_name_extension = splitext(url_path)
-    file_extension = path_name_extension[1]
+    file_path, file_extension = splitext(url_path)
     return file_extension
