4e0a5dd028ba74b607317017e6cab01e21ef236f
pn00m <lunueb@gmail.com>
2022-05-20 16:46:13 +0300

    Make new function 'fetch_images'

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,6 +5,13 @@ import requests
 from download_pictures import *
 
 
+def fetch_images(spacex_images, path):
+    for image_number, spacex_image_url in enumerate(spacex_images):
+        file_extension = define_file_extension(spacex_image_url)
+        spacex_photo_filepath = '{}/spacex{}{}'\
+            .format(path, image_number+1, file_extension)
+        download_pictures(spacex_image_url, spacex_photo_filepath)
+
 def main():
     path = 'images'
     spacex_api_link = 'https://api.spacexdata.com/v3/launches'
@@ -13,11 +20,7 @@ def main():
     response = requests.get(spacex_api_link, params=payload)
     spacex_images = response.json()[0]['links']['flickr_images']
     os.makedirs(path, exist_ok=True)
-    for image_number, spacex_image_url in enumerate(spacex_images):
-        file_extension = define_file_extension(spacex_image_url)
-        spacex_photo_filepath = '{}/spacex{}{}'\
-            .format(path, image_number+1, file_extension)
-        download_pictures(spacex_image_url, spacex_photo_filepath)
+    fetch_images(spacex_images, path)
 
 
 if __name__ == '__main__':
