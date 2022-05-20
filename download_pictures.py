414037a784b05b9416ed4c8ce6d6ea0408f6afb0
pn00m <lunueb@gmail.com>
2022-05-20 15:36:36 +0300

    Remove unused constant

--- download_pictures.py
+++ download_pictures.py
@@ -6,7 +6,6 @@ import requests
 
 def define_file_extension(url):
     url_path = urlsplit(url).path
-    path_name_extension = splitext(url_path)
     file_path, file_extension = splitext(url_path)
     return file_extension
 
